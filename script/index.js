8f02abba3e725543e66c97d3cf84b3660509d916
Vova Pankiv <vovapankiw@gmail.com>
2020-05-03 21:45:55 +0300

    added hive and sounds effect

--- script/index.js
+++ script/index.js
@@ -1,6 +1,7 @@
 const langEngEl = document.querySelector('.lang_en');
 const langRuEl = document.querySelector('.lang_ru');
 const avatarEl = document.querySelector('.avatar');
+const audioEl = document.querySelector('.beeSound');
 
 langEngEl.addEventListener('click', (e) => {
   avatarEl.setAttribute('data', './assets/images/avatar_eng.png');
@@ -8,4 +9,13 @@ langEngEl.addEventListener('click', (e) => {
 
 langRuEl.addEventListener('click', (e) => {
   avatarEl.setAttribute('data', './assets/images/avatar_soviet.png');
+});
+
+
+avatarEl.addEventListener('mouseenter', () => {
+  audioEl.play();
+});
+
+avatarEl.addEventListener('mouseleave', () => {
+  audioEl.pause();
 });
